062c57fabf13b593f98afef58a4739f16f5c7cca
Joe Li <joe.lea@foxmail.com>
2022-09-09 16:01:37 +0800

    redis 增加方法

--- facade/src/main/java/tech/icoding/sci/facade/components/redis/RedisService.java
+++ facade/src/main/java/tech/icoding/sci/facade/components/redis/RedisService.java
@@ -1,9 +1,13 @@
 package tech.icoding.sci.facade.components.redis;
 
+
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
+import java.time.Duration;
+
 /**
+ * Redis 服务
  * @author : Joe
  * @date : 2021/1/7
  */
@@ -18,6 +22,27 @@ public class RedisService {
 
     public void set(String key, String value){
         stringRedisTemplate.opsForValue().set(key, value);
+    }
+
+    /**
+     * 添加缓存并设置过期时间
+     * @param key
+     * @param value
+     * @param timeout
+     * @return
+     */
+    public Boolean set(String key, String value, Duration timeout){
+        return  stringRedisTemplate.opsForValue().setIfAbsent(key, value, timeout);
+    }
+
+    /**
+     * 获取并设置过期时间
+     * @param key
+     * @param timeout
+     * @return
+     */
+    public String get(String key, Duration timeout){
+        return stringRedisTemplate.opsForValue().getAndExpire(key, timeout);
     }
 
     public String get(String key) {
--- iservice/src/main/java/tech/icoding/sci/core/entity/RoleEntity.java
+++ iservice/src/main/java/tech/icoding/sci/core/entity/RoleEntity.java
@@ -40,7 +40,6 @@ public class RoleEntity extends BaseEntity<Long>{
 
     private Integer delFlag;
 
-
     @ManyToMany
     private Set<PermissionEntity> permissions;
 
